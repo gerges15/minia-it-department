250392b5c8c857699160d36d89684d901c81cdd3
Girgis Sami <jirgissamy78@gmail.com>
2025-02-27 20:55:04 +0200

    add Login Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,10 @@
 import "./App.css";
+import { Login } from "./pages/Login";
 
 function App() {
   return (
     <>
-      <div className="it">
-        <img src="Logos/Light_Logo_IT2.svg" alt="it logo" />
-        <p>It Department</p>
-      </div>
-      <div className="login">
-        <h1>Login</h1>
-        <div>
-          <p>Username</p>
-          <input type="text" name="user-name" id="user-name" />
-        </div>
-        <div>
-          <p>Password</p>
-          <input type="password" name="user-name" id="user-name" />
-        </div>
-        <p className="link">
-          <a href="">Forgot Password?</a>
-        </p>
-
-        <button>Let`s Go !</button>
-      </div>
+      <Login />
     </>
   );
 }
---
+++ src/pages/Login.jsx
@@ -0,0 +1,28 @@
+import "./Login.css";
+export const Login = function () {
+  return (
+    <>
+      <div className="it">
+        <img src="Logos/Light_Logo_IT2.svg" alt="it logo" />
+        <p>It Department</p>
+      </div>
+
+      <div className="login">
+        <h1>Login</h1>
+        <div>
+          <p>Username</p>
+          <input type="text" name="user-name" id="user-name" />
+        </div>
+        <div>
+          <p>Password</p>
+          <input type="password" name="user-name" id="user-name" />
+        </div>
+        <p className="link">
+          <a href="">Forgot Password?</a>
+        </p>
+
+        <button>Let`s Go !</button>
+      </div>
+    </>
+  );
+};
