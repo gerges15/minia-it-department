f4fdbf6910fd030f16d9a66e473f799b899ba755
Girgis Sami <jirgissamy78@gmail.com>
2025-02-27 20:34:09 +0200

    add login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,28 @@
-import { useState } from "react";
 import "./App.css";
 
 function App() {
   return (
     <>
-      <h1>Hello Department </h1>
-      <h1>Holly shit wait until the end </h1>
-      <h1>Just keep watching in silent 🤫</h1>
+      <div className="it">
+        <img src="Logos/Light_Logo_IT2.svg" alt="it logo" />
+        <p>It Department</p>
+      </div>
+      <div className="login">
+        <h1>Login</h1>
+        <div>
+          <p>Username</p>
+          <input type="text" name="user-name" id="user-name" />
+        </div>
+        <div>
+          <p>Password</p>
+          <input type="password" name="user-name" id="user-name" />
+        </div>
+        <p className="link">
+          <a href="">Forgot Password?</a>
+        </p>
+
+        <button>Let`s Go !</button>
+      </div>
     </>
   );
 }
