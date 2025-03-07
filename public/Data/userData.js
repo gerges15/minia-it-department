44df739eb397db903734c2d0d8f511e2fdbaa780
Girgis Sami <jirgissamy78@gmail.com>
2025-03-07 17:17:49 +0200

    add userData.js

---
+++ public/Data/userData.js
@@ -0,0 +1,13 @@
+export const passwordProps = {
+  id: "password",
+  type: "password",
+  label: "password",
+  name: "user-password",
+};
+
+export const userIdProps = {
+  id: "id",
+  type: "text",
+  label: "userId",
+  name: "user-id",
+};
