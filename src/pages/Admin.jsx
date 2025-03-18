4903636caf56f44cf01719dcda07cdbb799de8c2
Girgis Sami <jirgissamy78@gmail.com>
2025-03-18 19:22:31 +0200

    add Admin page and handel sidbar

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -1,13 +1,33 @@
+import Logo from "../components/Logo";
+import adminImg from "../../public/images/administrator.png";
+import "./Admin.css";
 function Admin() {
   return (
     <div className="admin">
-      <header className="header">
-        <nav> header</nav>
-      </header>
-      <aside>
-        <p>Sidebar</p>
+      <aside className="sid-bar scroll">
+        <div className="icon">
+          <img src={adminImg} alt="" />
+          Admin
+        </div>
+        <ul className="tabs">
+          <li className="tab-item">Home</li>
+          <li className="tab-item">Timetables</li>
+          <li className="tab-item">Manage</li>
+          <li className="tab-item">Registration rules</li>
+          <li className="tab-item">Mange Hall/Lab</li>
+          <li className="tab-item">Teaching Staff</li>
+          <li className="tab-item">Students</li>
+          <li className="tab-item">Logout</li>
+        </ul>
       </aside>
-      <main>
+
+      <header className="header">
+        <nav>
+          {" "}
+          <h1>Admin Dashboard</h1>
+        </nav>
+      </header>
+      <main className="main">
         <p>This is the main</p>
       </main>
     </div>
