24bbdbf733aa6e32d2db09d6551aa3e588780c0f
Girgis Sami <jirgissamy78@gmail.com>
2025-03-22 11:03:14 +0200

    add tab Icons

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -1,26 +1,78 @@
 import Logo from "../components/Logo";
-import adminImg from "../../public/images/administrator.png";
-import { RiAdminFill } from "react-icons/ri";
 
+import { RiAdminFill, RiHome2Fill } from "react-icons/ri";
 import "./Admin.css";
+import { FaTable, FaEdit, FaBookOpen } from "react-icons/fa";
+import { BiLogOut } from "react-icons/bi";
+import { MdManageHistory } from "react-icons/md";
+import { FaUserCog } from "react-icons/fa";
 function Admin() {
-  const iconSize = 50;
+  const largeIcon = 50;
+  const smallIcon = 25;
+  const mediumIcon = 37.5;
+  const iconColor = "#eff3ea";
   return (
     <div className="admin">
       <aside className="sid-bar scroll">
         <div className="icon">
-          <RiAdminFill size={iconSize} />
+          <RiAdminFill size={largeIcon} />
           Admin
         </div>
         <ul className="tabs">
-          <li className="tab-item">Home</li>
-          <li className="tab-item">Timetables</li>
-          <li className="tab-item">Manage</li>
-          <li className="tab-item">Registration rules</li>
-          <li className="tab-item">Mange Hall/Lab</li>
-          <li className="tab-item">Teaching Staff</li>
-          <li className="tab-item">Students</li>
-          <li className="tab-item">Logout</li>
+          <li className="tab-item">
+            <RiHome2Fill
+              size={smallIcon}
+              color={iconColor}
+              className="tab-icon"
+            />
+            Home
+          </li>
+          <li className="tab-item">
+            <FaTable size={smallIcon} color={iconColor} className="tab-icon" />
+            Timetables
+          </li>
+          <li className="tab-item">
+            <FaEdit size={smallIcon} color={iconColor} className="tab-icon" />
+            Manage
+          </li>
+
+          <li className="tab-item">
+            {" "}
+            <FaBookOpen
+              size={smallIcon}
+              color={iconColor}
+              className="tab-icon"
+            />
+            Registration rules
+          </li>
+          <li className="tab-item">
+            <MdManageHistory
+              size={smallIcon}
+              color={iconColor}
+              className="tab-icon"
+            />
+            Mange Hall/Lab
+          </li>
+          <li className="tab-item">
+            <FaUserCog
+              size={smallIcon}
+              color={iconColor}
+              className="tab-icon"
+            />
+            Teaching Staff
+          </li>
+          <li className="tab-item">
+            <FaUserCog
+              size={smallIcon}
+              color={iconColor}
+              className="tab-icon"
+            />
+            Students
+          </li>
+          <li className="tab-item">
+            <BiLogOut size={smallIcon} color={iconColor} className="tab-icon" />
+            Logout
+          </li>
         </ul>
       </aside>
 
