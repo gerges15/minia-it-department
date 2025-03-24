d3db12b201a30cb4a21732afb9965d5a89ace104
Girgis Sami <jirgissamy78@gmail.com>
2025-03-24 13:46:53 +0200

    add Home section to main

--- src/pages/admin/Main.jsx
+++ src/pages/admin/Main.jsx
@@ -2,7 +2,26 @@ import "../admin/Main.css";
 export default function Main() {
   return (
     <main className="main">
-      <h3> Lets go for a ? </h3>
+      <div className="home">
+        <p>
+          <span className="text--bold">First Name:</span> Girgis
+        </p>
+        <p>
+          <span className="text--bold">Second Name:</span> Samy
+        </p>
+        <p>
+          <span className="text--bold">Last Name:</span> Shafeq
+        </p>
+        <p>
+          <span className="text--bold">Id:</span> 303032032
+        </p>
+        <p>
+          <span className="text--bold">rol:</span> Admin
+        </p>
+        <p>
+          <span className="text--bold">phone number:</span> 01288348198
+        </p>
+      </div>
     </main>
   );
 }
