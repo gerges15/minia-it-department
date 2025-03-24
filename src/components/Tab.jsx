834287f5029f17ba58a6109e2e21766e42dfcc4b
Girgis Sami <jirgissamy78@gmail.com>
2025-03-24 11:43:28 +0200

    add Tab.css file

--- src/components/Tab.jsx
+++ src/components/Tab.jsx
@@ -1,3 +1,4 @@
+import "./Tab.css";
 export default function Tab({ style, className, children, handelLabel }) {
   return (
     <li style={{ ...style }} className={className} onClick={handelLabel}>
