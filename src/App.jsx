3a890dc9b1ce502b5dd26a02c1c85fddcd9df827
MoazEb <moazm942@gmail.com>
2025-04-08 10:58:57 +0200

    err page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from "react";
+import Error from "./Pages/Error";
 
 function App() {
   return (
-    <div className="relative w-full h-screen">
-      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-        <h1 className="font-poppins font-extrabold text-6xl"> Keep Waiting </h1>
-      </div>
-    </div>
+    // <div className="relative w-full h-screen">
+    //   <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+    //     <h1 className="font-poppins font-extrabold text-6xl"> Keep Waiting </h1>
+    //   </div>
+    // </div>
+    <Error />
   );
 }
 
