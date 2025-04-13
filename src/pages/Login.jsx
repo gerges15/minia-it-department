195841605c424be346abc7a7fa7f93d5e32b72b8
Girgis Sami <jirgissamy78@gmail.com>
2025-04-13 12:29:24 +0200

    Add react-redux Provider

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import LoginRightSide from '../components/LoginRightSide';
+import { Provider } from 'react-redux';
+import { loginStore } from '../state/store';
 const LoginPage = () => {
   return (
-    <div className="flex h-screen flex-col justify-center items-center  w-screen bg-[#f5f5f0]">
-      <LoginRightSide />
-    </div>
+    <Provider store={loginStore}>
+      <div className="flex h-screen flex-col justify-center items-center  w-screen bg-[#f5f5f0]">
+        <LoginRightSide />
+      </div>
+    </Provider>
   );
 };
 
