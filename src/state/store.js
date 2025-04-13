1c272e710ac2c7bf469585f3923fbfb031479f30
Girgis Sami <jirgissamy78@gmail.com>
2025-04-13 13:19:00 +0200

    Store loading reducer

--- src/state/store.js
+++ src/state/store.js
@@ -1,8 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
 import errorMsgReducer from './errorMsgSlice';
-
+import loadingReducer from './loadingSlice'
 export const loginStore = configureStore({
   reducer: {
     errorMsg: errorMsgReducer,
+    loading: loadingReducer
   },
 });
