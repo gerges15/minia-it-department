286b4a190a6d1fd0ddcb9cd7b7a4e361bc94a55e
Girgis Sami <jirgissamy78@gmail.com>
2025-04-13 14:29:06 +0200

    Store userReducer

--- src/state/store.js
+++ src/state/store.js
@@ -1,9 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import errorMsgReducer from './errorMsgSlice';
 import loadingReducer from './loadingSlice'
+import userReducer  from './userSlice';
+
 export const loginStore = configureStore({
   reducer: {
     errorMsg: errorMsgReducer,
-    loading: loadingReducer
+    loading: loadingReducer,
+    user:userReducer
   },
 });
