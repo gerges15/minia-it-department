dcedc1ef441908513470b09916ffa92007f086f7
Girgis Sami <jirgissamy78@gmail.com>
2025-04-13 15:37:44 +0200

    Refactor routes

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -4,21 +4,21 @@ import ForgotPassword from '../pages/ForgotPassword';
 import Login from '../pages/Login';
 import HomePage from '../pages/HomePage';
 import { Component } from 'react';
+import { Outlet } from 'react-router-dom';
+const RootLayout = () => <Outlet />;
 
-const rootRoute = {
-  path: '/',
-  errorElement: <NotFound />,
-  children: [
-    { index: true, Component: Login },
-    { path: 'forgot-password', Component: ForgotPassword },
-  ],
-};
-
-const loginRoute = {
-  path: 'login',
-  Component: Login,
-};
-
-const router = createBrowserRouter([rootRoute, loginRoute]);
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <RootLayout />,
+    errorElement: <NotFound />,
+    children: [
+      { index: true, element: <Login /> },
+      { path: 'forgot-password', element: <ForgotPassword /> },
+      { path: 'login', element: <Login /> },
+      { path: '*', element: <NotFound /> },
+    ],
+  },
+]);
 
 export default router;
