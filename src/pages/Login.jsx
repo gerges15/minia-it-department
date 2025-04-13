67fddf623ac0258371fbcea259652c9920db2c74
Girgis Sami <jirgissamy78@gmail.com>
2025-04-13 09:12:19 +0200

    Refactor

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,13 +1,9 @@
 import React from 'react';
-import LoginLeftSide from '../components/LoginLeftSide';
 import LoginRightSide from '../components/LoginRightSide';
-import LoginForm from '../components/molecules/LoginForm';
 const LoginPage = () => {
   return (
-    <div className="flex h-screen  w-screen bg-[#f5f5f0]">
-      <div className="flex justify-center w-full h-full">
-        <LoginRightSide />
-      </div>
+    <div className="flex h-screen flex-col justify-center items-center  w-screen bg-[#f5f5f0]">
+      <LoginRightSide />
     </div>
   );
 };
