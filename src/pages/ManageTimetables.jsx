3f40199923689712d48108dd8a656a6aef0adf95
Girgis Sami <jirgissamy78@gmail.com>
2025-04-22 14:16:24 +0200

    chore: reformat file

--- src/pages/ManageTimetables.jsx
+++ src/pages/ManageTimetables.jsx
@@ -1,11 +1,11 @@
 const ManageTimetables = () => {
-    return (
-        <div>
-            <h1 className="text-3xl font-bold text-gray-800 mb-6">ManageTimetable</h1>
-            <div className="bg-white p-6 rounded-xl shadow-md border border-gray-200">
-                <p className="text-gray-600">still under development</p>
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <h1 className="text-3xl font-bold text-gray-800 mb-6">ManageTimetable</h1>
+      <div className="bg-white p-6 rounded-xl shadow-md border border-gray-200">
+        <p className="text-gray-600">still under development</p>
+      </div>
+    </div>
+  );
 };
 export default ManageTimetables;
