1abce3400290162de3a8179dfa56dffed2346617
Girgis Sami <jirgissamy78@gmail.com>
2025-04-22 14:16:07 +0200

    refactor: replace Link component with button element

--- src/components/SidebarLogoutBtn.jsx
+++ src/components/SidebarLogoutBtn.jsx
@@ -1,19 +1,20 @@
-import { Link } from 'react-router-dom';
 import { FiLogOut } from 'react-icons/fi';
+import { useLogoutModalStore } from '../store/useLogoutModalStore';
+import LogoutModal from './Modal';
 
 export default function SidebarLogoutBtn() {
-  const defaultLinkStyle =
-    'text-gray-600 hover:bg-gray-100 hover:text-gray-900';
-
+  const defaultBtnStyle = 'text-gray-600 hover:bg-gray-100 hover:text-gray-900';
+  const { openModal } = useLogoutModalStore();
   return (
     <div className="flex-shrink-0 px-2 pt-4 pb-4 border-t border-gray-200">
-      <Link
-        to="/logout"
-        className={`group flex items-center px-3 py-2.5 text-sm rounded-md transition-colors duration-150 ease-in-out ${defaultLinkStyle}`}
+      <button
+        onClick={openModal}
+        className={`group flex items-center px-3 py-2.5 text-sm rounded-md transition-colors duration-150 ease-in-out cursor-pointer w-full ${defaultBtnStyle}`}
       >
         <FiLogOut className="mr-3 flex-shrink-0 h-5 w-5" aria-hidden="true" />
         Logout
-      </Link>
+      </button>
+      <LogoutModal />
     </div>
   );
 }
