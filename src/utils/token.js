184ea092b895b3cd1bf3efc2be5de62c35651a8e
Girgis Sami <jirgissamy78@gmail.com>
2025-04-28 20:02:47 +0300

    Add refreshToken method

--- src/utils/token.js
+++ src/utils/token.js
@@ -2,9 +2,13 @@ class Token {
   constructor(data) {
     this._data = data;
     this._accessToken = this._data.token;
+    this._refreshToken = this._data.refreshToken;
   }
 
   accessToken() {
+    return this._accessToken;
+  }
+  refreshToken() {
     return this._accessToken;
   }
 }
