2e929be4f54eb9f166f2a43958da652b930f83b6
Girgis Sami <jirgissamy78@gmail.com>
2025-04-29 18:33:19 +0300

    Test correctly store accessToken

--- tests/logic/inventory.test.js
+++ tests/logic/inventory.test.js
@@ -8,8 +8,32 @@ import {
   beforeEach,
   vi,
 } from 'vitest';
+import { Token } from '../../src/utils/token';
+import { Inventory } from '../../src/utils/inventory';
+import { setUserName, setUserPassword } from '../../src/store/useUserStore';
+import Cookies from 'js-cookie';
 
-let inventory;
-beforeAll(async => {
-  inventory = new Inventory();
+dotenv.config({ path: '.env' });
+
+describe('Test Inventor Class', async () => {
+  let inventory;
+  let tk;
+
+  beforeAll(async () => {
+    const usrPassword = process.env.VITE_TEST_PASSWORD;
+    const usrName = process.env.VITE_TEST_USERNAME;
+
+    setUserName(usrName);
+    setUserPassword(usrPassword);
+
+    tk = await Token.Create();
+    inventory = new Inventory(tk);
+  });
+
+  it('should store correct access token', async () => {
+    inventory.storeAccessToken();
+    const storedToken = Cookies.get('accessToken');
+
+    expect(storedToken).toBe(tk.accessToken);
+  });
 });
