fbf65f8ffc833f7deb927bdeb952db0ce8fe2cdf
Girgis Sami <jirgissamy78@gmail.com>
2025-04-29 18:33:52 +0300

    Add storeAccessToken

--- src/utils/inventory.js
+++ src/utils/inventory.js
@@ -1,3 +1,14 @@
-class Inventory {
-  constructor(data) {}
+import Cookies from 'js-cookie';
+export class Inventory {
+  constructor(data) {
+    this._data = data;
+  }
+
+  storeAccessToken() {
+    Cookies.set(
+      'accessToken',
+      this._data.accessToken,
+      this._data.refreshTokenExpireTime
+    );
+  }
 }
